675a022bf981c287630809bcbacfc0cd1d47038f
Tim Lightfoot <tlightfo@gmail.com>
2014-08-01 13:52:07 -0500

    Updated bat file to open newest workbook.

--- testRun.bat
+++ testRun.bat
@@ -1,1 +1,1 @@
-powershell .\referralSourceAutomationFromVBA.ps1 C:\Users\tlightfoot\Documents\Projects\PM_data_collection\PF_PM_Data_Collection_Workbook_7.22.14.xls
+powershell .\referralSourceAutomationFromVBA.ps1 C:\Users\tlightfoot\Documents\Projects\PM_data_collection\PF_PM_Data_Collection_Workbook_8.1.14.xls
